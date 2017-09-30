6a87d214d15b034ae72e7e5ddf9c3dfb551458d4
Matthew Hummer <biggreyson@gmail.com>
2017-09-30 14:31:35 -0600

    add cors; fix authorization check

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const gamesRoute = require('./routes/games.js');
 mongoose.connect('mongodb://localhost:27017/gameShop');
@@ -14,19 +15,24 @@ const db = mongoose.connection;
 
 
 db.on('open',()=>{
+  app.use(cors());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
   app.use('/', function(req,res,next){
-    let auth = req.headers.authorization.split(' ')[1];
-    let translated = new Buffer(auth, 'base64').toString('ascii');
-    let username = translated.split(':')[0];
-    let password = translated.split(':')[1];
-    // console.log('Username: ',username,'\nPassword: ',password);
-    if(username != process.env.USERNAME || password != process.env.PASSWORD){
+    if(!req.headers.authorization){
       res.sendStatus(401)
     }else{
-      next();
+      let auth = req.headers.authorization.split(' ')[1];
+      let translated = new Buffer(auth, 'base64').toString('ascii');
+      let username = translated.split(':')[0];
+      let password = translated.split(':')[1];
+      // console.log('Username: ',username,'\nPassword: ',password);
+      if(username != process.env.USERNAME || password != process.env.PASSWORD){
+        res.sendStatus(401)
+      }else{
+        next();
+      }
     }
   });
   app.get('/', function(req,res,next){
