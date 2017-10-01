e9430e78ed9b3fd74865848f9e31d68b5493b0a7
Matthew Hummer <biggreyson@gmail.com>
2017-10-01 10:17:06 -0600

    add some more game seeds

--- db/gameSeeds.js
+++ db/gameSeeds.js
@@ -3,26 +3,15 @@
   {
     "title": "Dark Souls",
     "price": 29.99,
-    "platforms": [
-      "xbox360",
-      "ps3"
-     ],
+    "platforms": ["xbox360","ps3"],
     "ESRB-rating": "Mature",
-    "rating": 5,
+    "rating": 4,
     "genre": "Action RPG",
     "cover": "http://darksouls.wikidot.com/local--files/start/na-cover-art.png"
   },{
     "title": "Minecraft",
     "price": 26.00,
-    "platforms": [
-      "xbox360",
-      "ps3",
-      "wiiU",
-      "switch",
-      "xbOne",
-      "ps4",
-      "pc"
-     ],
+    "platforms": ["xbox360", "ps3", "wiiU", "switch", "xbOne", "ps4", "pc"],
     "ESRB-rating": "Everyone",
     "rating": 4,
     "genre": "Survival Sandbox",
@@ -35,5 +24,21 @@
   	"rating": 5,
   	"genre": "Action Adventure",
     "cover": "https://img00.deviantart.net/93d7/i/2017/013/a/8/tloz_breath_of_the_wild_game_cover_2_by_gmivan-dav83sz.jpg"
+  },{
+    "title": "No Man's Sky",
+    "cover": "https://upload.wikimedia.org/wikipedia/en/6/67/No_Man%27s_Sky.jpg",
+    "price": 23.46,
+    "rating": 2,
+    "genre": "Action Adventure",
+    "ESRB-rating": "Teen",
+    "platforms": ["pc", "ps3"]
+  },{
+    "title": "Forza Motorsport 6",
+    "cover": "http://mms.businesswire.com/media/20150112006562/en/448626/5/Monument_Logo_Horizontal_Black.jpg",
+    "price": 35.99,
+    "rating": 5,
+    "genre": "Racing",
+    "ESRB-rating": "Everyone",
+    "platforms": ["pc", "xbOne"]
   }
 ]
